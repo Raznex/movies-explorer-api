36afdb62928acf473fc066a9babbc937054400b9
Raznex <dudya15@yandex.ru>
2023-06-19 17:37:55 +0300

    Обновил после линта

---
+++ models/user.js
@@ -0,0 +1,59 @@
+const isEmail = require('validator/lib/isEmail');
+const mongoose = require('mongoose');
+
+const bcrypt = require('bcrypt');
+const WrongEOP = require('../utils/errors/wrongEmailorPass');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Поле "email" должно быть заполнено'],
+      validate: [isEmail, 'Поле "email" неверно заполнено'],
+      index: { unique: true },
+    },
+    password: {
+      type: String,
+      required: [true, 'Поле "password" должно быть заполнено'],
+      select: false,
+    },
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+  },
+  {
+    versionKey: false,
+    toJSON: {
+      useProjection: true,
+    },
+    toObject: {
+      useProjection: true,
+    },
+  },
+  {
+    versionKey: false,
+    statics: {
+      findUserByCredentials(email, password) {
+        return this.findOne({ email })
+          .select('+password')
+          .then((user) => {
+            if (user) {
+              return bcrypt.compare(password, user.password).then((matched) => {
+                if (matched) {
+                  return user;
+                }
+                throw new WrongEOP('Неправильные почта или пароль');
+              });
+            }
+            throw new WrongEOP('Неправильные почта или пароль');
+          });
+      },
+    },
+  },
+
+);
+
+module.exports = mongoose.model('user', userSchema);
