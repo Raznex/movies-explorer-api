a56bb25df1a02536631d23835f18513ce3166830
Raznex <Dudya15@yandex.ru>
2023-07-17 20:37:42 +0300

    П8

--- middlewares/Cors.js
+++ middlewares/Cors.js
@@ -1,34 +1,34 @@
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'http://localhost:3000',
-  'http://127.0.0.1:3000',
-  'http://diploma.raznex.nomoredomains.rocks',
-  'https://diploma.raznex.nomoredomains.rocks',
-  'http://192.168.0.197:3000',
-];
-
-module.exports = (req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin); // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-  }
-
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
-
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS); // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Headers', requestHeaders); // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', allowedCors); // кукис с другого домена
-    return res.end(); // завершаем обработку запроса и возвращаем результат клиенту
-  }
-
-  return next();
-};
+// const allowedCors = [
+//   'https://praktikum.tk',
+//   'http://praktikum.tk',
+//   'http://localhost:3000',
+//   'http://127.0.0.1:3000',
+//   'http://diploma.raznex.nomoredomains.rocks',
+//   'https://diploma.raznex.nomoredomains.rocks',
+//   'http://192.168.0.197:3000',
+// ];
+//
+// module.exports = (req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     res.header('Access-Control-Allow-Origin', origin); // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+//   }
+//
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   const requestHeaders = req.headers['access-control-request-headers']; // сохраняем список заголовков исходного запроса
+//
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS); // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Headers', requestHeaders); // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Credentials', true);
+//     res.header('Access-Control-Allow-Origin', allowedCors); // кукис с другого домена
+//     return res.end(); // завершаем обработку запроса и возвращаем результат клиенту
+//   }
+//
+//   return next();
+// };
