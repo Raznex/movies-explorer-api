b0e52790c3319d78442664b423115da84ac17391
Raznex <Dudya15@yandex.ru>
2023-07-18 14:34:45 +0300

    П17

--- utils/joyValidation.js
+++ utils/joyValidation.js
@@ -36,7 +36,7 @@ module.exports = Object.freeze({
       description: Joi.string().required(),
       image: Joi.string().required(),
       trailerLink: Joi.string().required().regex(urlRegular),
-      thumbnail: Joi.string().required().regex(urlRegular),
+      thumbnail: Joi.string().required(),
       movieId: Joi.number().required(),
       nameRU: Joi.string().required(),
       nameEN: Joi.string().required(),
